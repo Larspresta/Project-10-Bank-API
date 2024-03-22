83ea9f95ab580fb3f57ff07d92e6930e4a6e4d31
LarsPresta <lauri.monni@gmail.com>
2024-03-22 07:46:26 +0100

    backend update

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,24 @@
-const mongoose = require('mongoose')
-const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+/* const mongoose = require("mongoose");
+const databaseUrl = process.env.DATABASE_URL || "mongodb://localhost/argentBankDB";
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(databaseUrl, { useNewUrlParser: true });
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
   }
-}
+}; */
+
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dbConnection = () => {
+  mongoose
+    .connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connection successful"))
+    .catch((err) => console.error("MongoDB connection error:", err));
+};
+
+module.exports = dbConnection;
